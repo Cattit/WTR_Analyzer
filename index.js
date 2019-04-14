82b8cd7bf1beb2f9c28669532e51cf4744b18adc
Cattit <ekaterina.titaeva@gmail.com>
2019-04-14 23:47:39 +0800

    new date (yesterday) to count mark

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const evaluationfifthDay = require("./newEvaluation/fifthDay.js")
 const dal = require("wtr-dal");
 const realWeatherForHours = require("./realWeatherForHours.js");
 const date = new Date() //сегодня (сейчас)
-date.setHours(0, 0, 0, 000)
+const yesterday = new Date(date.getFullYear(), date.getMonth(), date.getDate() - 1, 0, 0, 0, 000)
 
 async function getMark(id_source, id_location, depth_forecast) {
-    const yesterday = date.getDate() - 1
-    const dateSomeDaysAgo = new Date(date.getFullYear(), date.getMonth(), yesterday - depth_forecast);  //день 
+    const dateSomeDaysAgo = new Date(yesterday.getFullYear(), yesterday.getMonth(), yesterday.getDate() - depth_forecast);  //день 
     let markDay = []
-    for (let i = 0, date_start = new Date(date.getFullYear(), date.getMonth(), yesterday, 12, 0, 0, 000), date_end = new Date(date.getFullYear(), date.getMonth(), yesterday, 23, 59, 59, 999);
-        i < 2; i++ , date_start = new Date(date.getFullYear(), date.getMonth(), yesterday, 0, 0, 0, 000), date_end = new Date(date.getFullYear(), date.getMonth(), yesterday, 11, 59, 59, 999)) { // сначала на день, потом на ночь        
+    for (let i = 0, date_start = new Date(yesterday.getFullYear(), yesterday.getMonth(), yesterday.getDate(), 12, 0, 0, 000), date_end = new Date(yesterday.getFullYear(), yesterday.getMonth(), yesterday.getDate(), 23, 59, 59, 999);
+        i < 2; i++ , date_start = new Date(yesterday.getFullYear(), yesterday.getMonth(), yesterday.getDate(), 0, 0, 0, 000), date_end = new Date(yesterday.getFullYear(), yesterday.getMonth(), yesterday.getDate(), 11, 59, 59, 999)) { // сначала на день, потом на ночь        
         const dataForecast = await dal.getForecastDataLeftJoin(id_source, dateSomeDaysAgo, id_location, date_start, date_end)
         const masRealWeather = await dal.getweatherForHours(id_location, date_start, date_end) // реальная погода
         const dataRealWeather = await realWeatherForHours.collectWeather(masRealWeather)  //упорядоченная реальная погода
@@ -29,7 +28,7 @@ async function getDataFromDB() {
     const masDSL = await dal.getDepthSourceLocation() //DepthSourceLocation  
     for (let i = 0; i < masDSL.length; i++) {
         let markDay = await getMark(masDSL[i].id_source, masDSL[i].id_location, masDSL[i].depth)
-        await dal.saveRaiting(masDSL[i].id_source, markDay, masDSL[i].depth, date, masDSL[i].id_location)
+        await dal.saveRaiting(masDSL[i].id_source, markDay, masDSL[i].depth, yesterday, masDSL[i].id_location)
     }
 }
 
